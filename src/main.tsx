b5a9a05bb8030373a7be34d83b65e2a6529224c9
IgorBragaLessa <igorbragalessal@gmail.com>
2026-05-03 00:40:01 -0300

    Projeto portfólio atualizado

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GlobalStyle } from "@/styles/Global";
+import { globalStyles } from "@/styles/Global";
 import { Home } from "@/pages/home";
 import { NavBar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 
+globalStyles();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle>
-      <NavBar />
-      <Home />
-      <Footer />
-    </GlobalStyle>
+    <NavBar />
+    <Home />
+    <Footer />
   </React.StrictMode>
 );
